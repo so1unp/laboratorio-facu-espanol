bb31ac5ecac6a22b2802cdb3032a0debf4b4da6d
Facundo <facuespaniol@gmail.com>
2024-03-17 20:42:19 -0300

    Resolviendo laboratorio 1

--- lab-01/histograma.c
+++ lab-01/histograma.c
@@ -4,7 +4,30 @@
 int main(int argc, char *argv[])
 {
     // Agregar código aquí.
-
+	const int MAX=10;
+	int cant[MAX];
+	int contador=0;
+	int i;
+	for (i=0;i<MAX; i++){
+		cant[i]=0;	
+	}
+	char entrada;
+	while ((entrada=getchar())!=EOF){
+		if ((entrada!=' ') && (entrada!='\n')){
+			contador++;
+		} else{
+			cant[contador]++;
+			contador=0;
+		}
+	}
+	int j;
+	for (i=1; i<MAX; i++){
+		printf("%d",i);
+		for (j=0; j<cant[i]; j++){
+			printf("*");
+		} 
+		printf("\n");
+	}
     // Termina la ejecución del programa.
     exit(EXIT_SUCCESS);
 }
