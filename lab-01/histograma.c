c70f36e1cf79d29150bea3c9371f925e39052574
Facundo <facuespaniol@gmail.com>
2024-03-20 21:28:49 -0300

    Resolviendo laboratorio 1

--- lab-01/histograma.c
+++ lab-01/histograma.c
@@ -3,8 +3,7 @@
 
 int main(int argc, char *argv[])
 {
-    // Agregar código aquí.
-	const int MAX=10;
+	int MAX=2000000; //MAX-1 es el maximo tamaño del histograma
 	int cant[MAX];
 	int contador=0;
 	int i;
@@ -20,8 +19,18 @@ int main(int argc, char *argv[])
 			contador=0;
 		}
 	}
+	int maximo=MAX;
+	int encontrado=0;
+	for (i=MAX-1 ;i>=1 && encontrado!=1; i--){
+		if(cant[i]==0){
+			maximo=i;
+		}
+		else{
+			encontrado=1;
+		}
+	}
 	int j;
-	for (i=1; i<MAX; i++){
+	for (i=1; i<maximo; i++){
 		printf("%d",i);
 		for (j=0; j<cant[i]; j++){
 			printf("*");
