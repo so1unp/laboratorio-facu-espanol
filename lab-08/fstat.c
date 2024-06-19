cfddf21eb31152fa304ff249b7657aa027183c15
Facundo <facuespaniol@gmail.com>
2024-06-18 21:51:56 -0300

    Laboratorio 8 resuelto

--- lab-08/fstat.c
+++ lab-08/fstat.c
@@ -1,9 +1,62 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <time.h>
+#include <sys/types.h>
+
+
+void print_file_type(mode_t mode)
+{
+    switch (mode & S_IFMT) {
+        case S_IFREG:
+            printf("Tipo de archivo: Archivo regular\n");
+            break;
+        case S_IFDIR:
+            printf("Tipo de archivo: Directorio\n");
+            break;
+        case S_IFCHR:
+            printf("Tipo de archivo: Dispositivo de caracteres\n");
+            break;
+        case S_IFBLK:
+            printf("Tipo de archivo: Dispositivo de bloques\n");
+            break;
+        case S_IFIFO:
+            printf("Tipo de archivo: FIFO (tubería con nombre)\n");
+            break;
+        case S_IFLNK:
+            printf("Tipo de archivo: Enlace simbólico\n");
+            break;
+        case S_IFSOCK:
+            printf("Tipo de archivo: Socket\n");
+            break;
+        default:
+            printf("Tipo de archivo: Desconocido\n");
+            break;
+    }
+}
 
 int main(int argc, char *argv[])
 {
-    // Agregar código aquí.
+    if (argc != 2)
+    {
+        printf("Cantidad de parametros incorrecta\n");
+        exit(EXIT_FAILURE);
+    }
+    struct stat file_stat;
+    if (stat(argv[1], &file_stat) == -1)
+    {
+        perror("stat");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("ID del usuario propietario: %u\n", file_stat.st_uid);
+    print_file_type(file_stat.st_mode);
+    printf("Número de i-nodo: %lu\n", file_stat.st_ino);
+    printf("Tamaño en bytes: %ld\n", file_stat.st_size);
+    printf("Número de bloques que ocupa en disco: %ld\n", file_stat.st_blocks);
+    printf("Tiempo de última modificación: %s", ctime(&file_stat.st_mtime));
+    printf("Tiempo de último acceso: %s", ctime(&file_stat.st_atime));
 
     // Termina la ejecución del programa.
     exit(EXIT_SUCCESS);
--- lab-08/write_bytes.c
+++ lab-08/write_bytes.c
@@ -10,7 +10,8 @@ int main(int argc, char *argv[])
     char *buf;
     int sync, fd, openFlags;
 
-    if (argc != 5) {
+    if (argc != 5)
+    {
         fprintf(stderr, "Uso: %s num-bytes buf-size sync archivo\n", argv[0]);
         exit(EXIT_FAILURE);
     }
@@ -20,7 +21,8 @@ int main(int argc, char *argv[])
     sync = atoi(argv[3]);
 
     buf = malloc(bufSize);
-    if (buf == NULL) {
+    if (buf == NULL)
+    {
         perror("malloc");
         exit(EXIT_FAILURE);
     }
@@ -28,25 +30,36 @@ int main(int argc, char *argv[])
     openFlags = O_CREAT | O_WRONLY;
 
     fd = open(argv[4], openFlags, S_IRUSR | S_IWUSR);
-    if (fd == -1) {
+    if (fd == -1)
+    {
         perror("open");
         exit(EXIT_FAILURE);
     }
 
-    // Esta condicion esta presente para evitar el warning que dice que sync no esta siendo usada.
-    // Se puede eliminar cuando se implemente fsync() y fdatasync()
-    sync = sync == 1 ? 0 : 1;
-
-    for (totWritten = 0; totWritten < numBytes; totWritten += thisWrite) {
+    for (totWritten = 0; totWritten < numBytes; totWritten += thisWrite)
+    {
         thisWrite = numBytes - totWritten > bufSize ? bufSize : numBytes - totWritten;
 
-        if (write(fd, buf, thisWrite) != thisWrite) {
+        if (write(fd, buf, thisWrite) != thisWrite)
+        {
             perror("write");
             exit(EXIT_FAILURE);
         }
+        switch (sync)
+        {
+        case 0:
+            break;
+        case 1:
+            fsync(fd);
+            break;
+        case 2:
+            fdatasync(fd);
+            break;
+        }
     }
 
-    if (close(fd) == -1) {
+    if (close(fd) == -1)
+    {
         perror("close");
         exit(EXIT_FAILURE);
     }
